dc2ef934d9d79faea9d9b4480569527617ece982
Jerome <jeromesardoma@gmail.com>
2018-08-31 23:14:23 -0700

    added undo like functionality

--- src/Card.js
+++ src/Card.js
@@ -10,8 +10,11 @@ class Card extends React.Component {
 			name: "Marshall Mathers",
 			datePosted: "3 hrs",
 			likes: 0,
+			liked: false,
 			comments: 0,
-			shares: 0
+			commented: false,
+			shares: 0,
+			shared: false
 		};
 		
 		this.addLike = this.addLike.bind( this );
@@ -26,37 +29,43 @@ class Card extends React.Component {
 	
 	addLike () {
 		this.setState({
-			likes: this.state.likes + 1
+			likes: this.state.likes + 1,
+			liked: true
 		})
 	};
 	
 	unlike () {
 		this.setState({
-			likes: this.state.likes -1
+			likes: this.state.likes -1,
+			liked: false
 		})
 	}
 	
 	addComment () {
 		this.setState({
-			comments: this.state.comments + 1
+			comments: this.state.comments + 1,
+			commented: true
 		})
 	};
 	
 	uncomment () {
 		this.setState({
-			comments: this.state.comments - 1
+			comments: this.state.comments - 1,
+			commented: false
 		})
 	}
 	
 	addShare () {
 		this.setState({
-			shares: this.state.shares + 1
+			shares: this.state.shares + 1,
+			shared: true
 		})
 	}
 	
 	unshare () {
 		this.setState({
-			shares: this.state.shares - 1
+			shares: this.state.shares - 1,
+			shared: false
 		})
 	}
 	
@@ -64,16 +73,37 @@ class Card extends React.Component {
 		const userName = this.state.name; 
 		const datePosted = this.state.datePosted;
 		const likes = this.state.likes;
+		const liked = this.state.liked;
 		const comments = this.state.comments;
+		const commented = this.state.commented;
 		const shares = this.state.shares;
+		const shared = this.state.shared;
 		
 		function showReactions () {
-			if( likes === 0 && comments === 0 && shares === 0 ) {
-				return '';
-			} 
-			return [likes, comments, shares].filter( e => e > 0 ).map(
-				e => e + e.toString()
-			)
+			const reactions = {
+				"likes": likes,
+				"comments": comments,
+				"shares": shares
+			};
+			let result = [];
+			
+			// show reactions in lower right corner of card, only if there are non
+			// zero reactions tracked
+			for ( let k in reactions ) {
+				if ( reactions[k] > 0 ) {
+					// show singular reaction name if there is only one reaction tracked
+					if ( reactions[k] === 1 ) {
+						result.push( reactions[k] + " " + k.slice(0, -1) );
+					// else, give plural form of reaction for more than one reaction
+					} else {
+						result.push( reactions[k] + " " + k );
+					}
+				}
+			}
+			
+			// if there's more than one reaction tracked, join the array with a space, else
+			// just show the singular reaction
+			return result.length > 1 ? result.join(" ") : result;
 		}
     
 		return (
@@ -98,13 +128,13 @@ class Card extends React.Component {
 				</div>
 				<hr style={{ margin: "0px 10px" }} />
 				<div id="reactionDiv" style={ styles.reactionDivStyle }>
-					<button id="like" onClick={ this.addLike } style={{ padding: "5px 10px" }}>
+					<button id="like" onClick={ liked ? this.unlike : this.addLike } style={{ padding: "5px 10px" }}>
 						Like
 					</button>
-					<button id="comment" onClick={ this.addComment } style={{ padding: "10px 5px" }}>
+					<button id="comment" onClick={ commented ? this.uncomment : this.addComment } style={{ padding: "10px 5px" }}>
 						Comment
 					</button>
-					<button id="share" onClick={ this.addShare } style={{ padding: 10 }}>
+					<button id="share" onClick={ shared ? this.unshare : this.addShare } style={{ padding: 10 }}>
 						<span>Share</span>
 					</button>
 					<div id="reactionCounter" style={ styles.reactionCounterStyle }>
